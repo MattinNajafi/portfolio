dff1cba1e0b346587c31467e81a1cc0d0b2699c1
Mattin Najafi <mattin-najafi@hotmail.com>
2021-01-07 15:21:39 +0100

    animations done

--- javascript/main.js
+++ javascript/main.js
@@ -110,12 +110,42 @@ function typeWriter(){
             document.querySelector(`.text-content-buttons-${count}`).appendChild(buttonDiv)
             
             document.querySelector('#continue').addEventListener('click', function(){
-                cursor.parentNode.removeChild(cursor)
-                count++
-                typeWriterIndex = 0
-                typeWriter()
-                buttonDiv.parentNode.removeChild(buttonDiv)
+                if(count === 0){
+                    buttonDiv.parentNode.removeChild(buttonDiv)
+                    let img = document.createElement('img')
+                    img.classList.add('explosion')
+                    img.src = '../img/explosion.gif'
+                    document.querySelector(`.text-content-buttons-${count}`).appendChild(img)
+                    setTimeout(function(){
+                        count++
+                        typeWriterIndex = 0
+                        typeWriter()
+                        cursor.parentNode.removeChild(cursor)
+                        img.parentNode.removeChild(img)
+
+                    },500)
+                }
+                if(count === 1){
+                    let img = document.createElement('img')
+                    img.classList.add('explosion')
+                    img.src = '../img/mario.gif'
+                    document.querySelector(`.text-content-buttons-${count}`).appendChild(img)
+                    buttonDiv.classList.add('mario')
+                    img.style.marginTop = '-5px'
+                    setTimeout(function(){
+                        buttonDiv.parentNode.removeChild(buttonDiv)
+                        count++
+                        typeWriterIndex = 0
+                        typeWriter()
+                        cursor.parentNode.removeChild(cursor)
+                        img.parentNode.removeChild(img)
+
+                    },700)
+                }
             })
+        }
+        else{
+            cursor.parentNode.removeChild(cursor)
         }
 
 
