2f9fa0754b2247c9e8b2cc03478c518d7d4e7008
Mattin Najafi <mattin-najafi@hotmail.com>
2021-01-21 18:13:48 +0100

    updated descriptions

--- src/components/work/workdata.js
+++ src/components/work/workdata.js
@@ -2,32 +2,32 @@ const workData = [
   {
     id: 1,
     header: "Jannes Tapeter",
-    description: "Gjorde si och så sohc så si och saå sdet",
+    description:
+      "Final project together with a group selected by the school in dynamic web development, used mongodb as backend",
   },
   {
     id: 2,
     header: "ColorSplat",
-    description: "Gjorde si och så sohc så si och saå sdet",
+    description: "Final project with a group of 5 in JavaScript",
   },
   {
     id: 3,
     header: "M&M Weather",
-    description: "Gjorde si och så sohc så si och saå sdet",
-  },
+    description: "First react project where API connection was mandatory, project done with a classmate"
   {
     id: 4,
     header: "Bowl & Burrito",
-    description: "Gjorde si och så sohc så si och saå sdet",
+    description: "Just a shallow of a shop, project in UX Design, worked with mockup and wireframe",
   },
   {
     id: 5,
     header: "Sneaker Shop",
-    description: "Gjorde si och så sohc så si och saå sdet",
+    description: "First webshop created together with a classmate, no backend included",
   },
   {
     id: 6,
     header: "CV",
-    description: "Gjorde si och så sohc så si och saå sdet",
+    description: "First portfolio created, on of the first projects in JavaScript",
   },
 ];
 
